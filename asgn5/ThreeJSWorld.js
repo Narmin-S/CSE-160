2d95d58dba93cda296dba2bea95708ef707514a0
Narmin "Valentina" Serrano <74797681+Narmin-S@users.noreply.github.com>
2025-06-05 16:35:05 -0700

    Last update!

--- asgn5/ThreeJSWorld.js
+++ asgn5/ThreeJSWorld.js
@@ -50,10 +50,27 @@ function main() {
             this.object[this.prop].set(hexString);
         }
     }
+    class DegRadHelper {
+        constructor(obj, prop) {
+            this.obj = obj;
+            this.prop = prop;
+        }
+        get value() {
+            return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+        }
+        set value(v) {
+            this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+        }
+    }
 
     function updateCamera() {
         camera.updateProjectionMatrix();
     }
+
+    function updateSpotLight() {
+    spot_light.target.updateMatrixWorld();
+    spot_helper.update();
+}
  
     const gui = new GUI();
     const cameraFolder = gui.addFolder('Camera');
@@ -99,6 +116,21 @@ function main() {
         hemisphereFolder.addColor(new ColorGUIHelper(hemi_light, 'color'), 'value').name('skyColor');
         hemisphereFolder.addColor(new ColorGUIHelper(hemi_light, 'groundColor'), 'value').name('groundColor');
         hemisphereFolder.add(hemi_light, 'intensity', 0, 5, 0.01);
+
+        const spot_color = 0xFFFFFF;
+        const spot_intensity = 150;
+        const spot_light = new THREE.SpotLight(spot_color, spot_intensity);
+        scene.add(spot_light);
+        scene.add(spot_light.target);
+
+        const spot_helper = new THREE.SpotLightHelper(spot_light);
+        scene.add(spot_helper);
+
+        const spotFolder = gui.addFolder('SpotLight');
+        spotFolder.add(new DegRadHelper(spot_light, 'angle'), 'value', 0, 90).name('angle').onChange(updateSpotLight);
+        spotFolder.add(spot_light, 'penumbra', 0, 1, 0.01);
+        spotFolder.add(spot_light.position, 'x', -50, 50).name('pos x').onChange(updateSpotLight);
+        spotFolder.add(spot_light.position, 'z', -50, 50).name('pos z').onChange(updateSpotLight);
     {
         const planeSize = 60;
         const loader = new THREE.TextureLoader();
@@ -168,6 +200,103 @@ function main() {
     textured_orb.position.z = 4;
     scene.add(textured_orb);
     dragon_powers.push(textured_orb);
+
+    const diamondGeometry = new THREE.OctahedronGeometry(0.8); // size 0.8, adjust as needed
+    const diamondMaterial = new THREE.MeshPhongMaterial({
+        color: 0xB9F2FF, 
+        shininess: 100,
+        transparent: true,
+        opacity: 0.8,
+        specular: 0xffffff
+    });
+    const diamond = new THREE.Mesh(diamondGeometry, diamondMaterial);
+    diamond.position.set(0, 12, 4);
+    scene.add(diamond);
+
+    function makediamond(d20geometry, color, x, y, z) {
+        const material = new THREE.MeshPhongMaterial({
+            color: color,
+            shininess: 100,
+            transparent: true,
+            opacity: 0.8,
+            specular: 0xffffff
+        });
+
+        const diamond = new THREE.Mesh(d20geometry, material);
+        scene.add(diamond);
+        
+        diamond.position.x = x;
+        diamond.position.y = y;
+        diamond.position.z = z;
+        
+        return diamond;
+    }
+
+    const shines = [
+        makediamond(diamondGeometry, 0xC0FA9A, -5, 3, 8),
+        makediamond(diamondGeometry, 0xaa8844,  3, 3, -4),
+        makediamond(diamondGeometry, 0xB9F2FF,4, 3, 8),
+        makediamond(diamondGeometry, 0xAFB7C9,8, 4, 10),
+        makediamond(diamondGeometry, 0x1C6104,-5, 10, 2),
+        makediamond(diamondGeometry, 0xF85C47,-3, 8, -2),
+    ];
+
+    const radiusTop = 1;  
+    const radiusBottom = 1;  
+    const height = 4;  
+    const radialSegments = 12;  
+
+    const cyngeometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+    const cynMaterial = new THREE.MeshPhongMaterial({color: 0xB9F2FF});
+    
+    const cynlinder = new THREE.Mesh(cyngeometry, cynMaterial);
+    cynlinder.position.set(4, 0, 8);
+    scene.add(cynlinder);
+
+    function makecyns(cyngeometry, color, x, y, z) {
+        const material = new THREE.MeshPhongMaterial({color});
+
+        const cynli = new THREE.Mesh(cyngeometry, material);
+        scene.add(cynli);
+        
+        cynli.position.x = x;
+        cynli.position.y = y;
+        cynli.position.z = z;
+        
+        return cynli;
+    }
+
+    const cynslinders = [
+        makecyns(cyngeometry, 0x8844aa, -5, 0, 8),
+        makecyns(cyngeometry, 0xaa8844,  3, 0, -4),
+    ];
+    const radius = 1;
+    const d20geometry = new THREE.IcosahedronGeometry( radius );
+    const d20Material = new THREE.MeshPhongMaterial({color: 0xB9F2FF});
+    const die = new THREE.Mesh(d20geometry, d20Material);
+    die.position.set(-2, 1, 10);
+    scene.add(die);
+
+    function makedie(d20geometry, color, x, y, z) {
+        const material = new THREE.MeshPhongMaterial({color});
+
+        const die = new THREE.Mesh(d20geometry, material);
+        scene.add(die);
+        
+        die.position.x = x;
+        die.position.y = 1;
+        die.position.z = z;
+        
+        return die;
+    }
+
+    const dieset = [
+        makedie(d20geometry, 0x8844aa, -8, 1, 8),
+        makedie(d20geometry, 0xaa8844,  -3, 0, -4),
+        makedie(d20geometry,0x1D678E,  -5, 1, -1),
+        makedie(d20geometry, 0xFF46A2,  2, 0, 6),
+        makedie(d20geometry, 0xD08E19,  -6, 10, 10),
+    ];
 
     {
         {
